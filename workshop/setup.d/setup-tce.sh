c3684f2477cbdf44963f6cbddfbb2707fb7a4ba8
Brian McClain <brianmmcclain@gmail.com>
2022-05-10 01:41:07 -0400

    Don't download TCE for now

--- workshop/setup.d/setup-tce.sh
+++ workshop/setup.d/setup-tce.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-set -x
+# set -x
 
-TCE_VERSION=0.12.0-rc.2
-TCE_IMAGE_VERSION=0.12.0-rc2
+# TCE_VERSION=0.12.0-rc.2
+# TCE_IMAGE_VERSION=0.12.0-rc2
 
-curl -L https://github.com/vmware-tanzu/community-edition/releases/download/v$TCE_VERSION/tce-linux-amd64-v$TCE_VERSION.tar.gz -o tce-linux-amd64-v$TCE_VERSION.tar.gz
-tar -xf tce-linux-amd64-v$TCE_VERSION.tar.gz
-mkdir bin
-cd tce-linux-amd64-v$TCE_VERSION
+# curl -L https://github.com/vmware-tanzu/community-edition/releases/download/v$TCE_VERSION/tce-linux-amd64-v$TCE_VERSION.tar.gz -o tce-linux-amd64-v$TCE_VERSION.tar.gz
+# tar -xf tce-linux-amd64-v$TCE_VERSION.tar.gz
+# mkdir bin
+# cd tce-linux-amd64-v$TCE_VERSION
 
-cp tanzu /home/eduk8s/bin/tanzu
-./install.sh
+# cp tanzu /home/eduk8s/bin/tanzu
+# ./install.sh
 
-tanzu package repository add tce-repo --url projects.registry.vmware.com/tce/main:$TCE_IMAGE_VERSION
+# tanzu package repository add tce-repo --url projects.registry.vmware.com/tce/main:$TCE_IMAGE_VERSION
